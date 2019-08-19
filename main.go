40b786a6ea8141587b6e2e0a34b86981ebc41185
wycstar <wycstar@live.com>
2019-08-20 00:33:59 +0800

    [REG]20190819

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"main/config"
-	"main/model"
+	"net"
 )
 
 func main() {
 	config.CONFIG.Init("/home/wyc/code/go/dhtgo/config/config.yaml")
 	fmt.Println(config.CONFIG)
-	model.Upsert()
+	addr, err := net.ResolveUDPAddr("udp", "router.bittorrent.com:6881")
+	if err != nil {
+		fmt.Println(err)
+		log.Panic(err)
+	}
+	fmt.Println(addr.IP)
+	//listenAddr := new(net.UDPAddr)
+	//listener, err := net.ListenUDP("udp", listenAddr)
+	//fmt.Println(listener)
 }
